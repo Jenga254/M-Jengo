62d93d9160bf53c6990c89b623bfcf0c859e397e
kiplogos <logosk98@gmail.com>
2025-01-13 20:08:51 +0300

    Fixed static files

--- api/server.js
+++ api/server.js
@@ -25,6 +25,7 @@ app.use(
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(express.static(path.join(__dirname, "../public")));
+app.use(express.static(path.join(__dirname, "../views")));
 
 // Connect to MongoDB
 mongoose
