48d2dd084e1b62487cf59ddc3da5a6e5ecba6e7e
kiplogos <logosk98@gmail.com>
2025-01-22 20:38:35 +0300

    updated server.js

--- api/server.js
+++ api/server.js
@@ -17,7 +17,7 @@ const port = 3000;
 // Enable CORS for all origins (for development only)
 app.use(
   cors({
-    origin: ["https://m-jengo-7cq2-frontend.vercel.app"],
+    origin: ["https://m-jengo-backend.vercel.app"],
     methods: ["POST", "GET"],
     credentials: true,
   })
