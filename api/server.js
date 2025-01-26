15da8759fbbdf9c8e39bd9e091d8c8dd46420137
kiplogos <logosk98@gmail.com>
2025-01-26 14:15:05 +0300

    Update vercel

--- api/server.js
+++ api/server.js
@@ -16,6 +16,10 @@ app.use(
     credentials: true, // If you need cookies or credentials
   })
 );
+app.use((req, res, next) => {
+  console.log(`Incoming request: ${req.method} ${req.url}`);
+  next();
+});
 
 
 const app = express();
